5101f4c016c6dff8c92e21aecbf62e6671d2e6c6
Adriel <adriel.alegrette@gmail.com>
2025-02-25 20:18:07 -0300

    HelloWord

--- helloword.java
+++ helloword.java
@@ -1,1 +1,9 @@
-dsadas
+public class Hello {
+    public static void helloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        helloWorld();
+    }
+}
